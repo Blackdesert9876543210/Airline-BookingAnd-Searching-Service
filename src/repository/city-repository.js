ff5e892200e65b6f5c36a70dae8ab547b6ae40a1
AshimBarman <ashlabbarman@gmail.com>
2023-04-11 10:22:22 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,1 +1,55 @@
-const{ City } = require('../models/index')
+const{ City } = require('../models/index');
+
+class CityRepository {
+    async createCity({ name }){
+        try {
+            const city = await City.create({
+                name : name
+               });
+               return city;
+        } catch (error) {
+            console.log("some error occured in the repository layer");
+            throw {error}; 
+        }
+       
+    }
+
+    async deleteCity( cityId ){
+        try {
+           await City.destroy({
+                where:{
+                    id : cityId
+                }
+            });
+            
+        } catch (error) {
+            console.log("some error occured in the repository layer");
+            throw {error}; 
+        }
+    }
+
+    async updateCity(cityId,data){
+        try {
+            const city = await City.update(data,{
+                where:{
+                    id : cityId
+                }
+            })
+            
+        } catch (error) {
+            console.log("some error occured in the repository layer");
+            throw {error}; 
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("some error occured in the repository layer");
+            throw {error}; 
+        }
+    }
+}
+module.exports = CityRepository;
