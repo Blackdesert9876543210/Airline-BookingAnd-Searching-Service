e96dfcc28d1038607096d7a9f762b91dba5639fb
AshimBarman <ashlabbarman@gmail.com>
2023-04-22 19:34:53 +0530

    Added flight-create request mmiddlewares

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const CityController = require('../../controllers/city-controllers');
 const FlightController = require('../../controllers/flight-controller');
 const AirportController = require('../../controllers/airport-controller');
+const { FlightMiddlewares } = require('../../middlewares/index');
 
 const router = express.Router();
 
@@ -11,7 +12,10 @@ router.get('/city/:id', CityController.get);
 router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update);
 
-router.post('/flights',FlightController.create);
+router.post(
+    '/flights',
+    FlightMiddlewares.validateCreateFlight,
+    FlightController.create);
 // router.get('/flights', FlightController.getAll);
 
 router.post('/airports', AirportController.create);
